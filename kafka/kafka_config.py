ab561ab7a394bda274055e0c5876e11914811800
Oleh Lokshyn <oloks@softserveinc.com>
2019-11-25 17:00:05 +0200

    Configured Step 1 to connect to Confluent Cloud
    1. step1_pull_tweets now reads tweets from
       Twitter API and pushes them in tweets kafka
       topic on Confluent Cloud in AVRO format
       using Schema Registry.
    2. Added kafka/config/secrets.yaml for storing
       Confluent Cloud API Keys.

--- kafka/kafka_config.py
+++ kafka/kafka_config.py
@@ -9,18 +9,20 @@ CONFIG_DIR = os.path.join(SCRIPT_DIR, 'config')
 
 def _load_config(filename):
     with open(os.path.join(CONFIG_DIR, filename)) as config_file:
-        return yaml.load(config_file) or {}
+        return yaml.load(config_file, Loader=yaml.SafeLoader) or {}
 
 
 def load_producer_config():
     return {
         **_load_config('common.yaml'),
-        **_load_config('producer.yaml')
+        **_load_config('producer.yaml'),
+        **_load_config('secrets.yaml')
     }
 
 
 def load_consumer_config():
     return {
         **_load_config('common.yaml'),
-        **_load_config('consumer.yaml')
+        **_load_config('consumer.yaml'),
+        **_load_config('secrets.yaml')
     }
